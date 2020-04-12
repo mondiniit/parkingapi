fd93c2efcdc9689962d6937a381f71b923950138
dmondini <dg.mondini@gmail.com>
2020-04-12 01:01:24 -0400

    [DM]feat: resolve commit, change service, change controller.

--- src/main/java/com/mondini/parkingapi/controller/ParkController.java
+++ src/main/java/com/mondini/parkingapi/controller/ParkController.java
@@ -1,5 +1,7 @@
 package com.mondini.parkingapi.controller;
 
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,35 +16,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mondini.parkingapi.entity.Park;
 import com.mondini.parkingapi.model.ParkModel;
-<<<<<<< HEAD
-=======
 import com.mondini.parkingapi.service.ParkService;
 
->>>>>>> 765a7734898ff0ebdb0a6a097757996ec960258e
 
 @Controller
 @RequestMapping("/park")
 public class ParkController {
 
 	private static final Log LOG = LogFactory.getLog(ParkController.class);
-<<<<<<< HEAD
+	
+	@Autowired
+	@Qualifier("parkServiceImpl")
+	private ParkService parkService;
 
 	// ADDIN
 	@PostMapping("/addin")
 	public void addIn(@ModelAttribute(name = "park") ParkModel parkModel) {
 		LOG.info("--ADD-IN");
-=======
-	
-	@Autowired
-	@Qualifier("parkServiceImpl")
-	private ParkService parkService;
+	}
 	
 	//ADDIN
 	@PostMapping("/addin/{park}")
 	public void addIn(@ModelAttribute(name="park") ParkModel parkModel, Model model) {
 		
 	
->>>>>>> 765a7734898ff0ebdb0a6a097757996ec960258e
 	}
 
 	// ADDOut
@@ -53,14 +50,8 @@ public class ParkController {
 
 	// List
 	@GetMapping("/entrances")
-<<<<<<< HEAD
-	public List<ParkModel> getAllParks() {
-
-		return null;
-=======
 	public @ResponseBody Park getAllParks(){
 		return parkService.getVisitor(); 
->>>>>>> 765a7734898ff0ebdb0a6a097757996ec960258e
 	}
 
 }
